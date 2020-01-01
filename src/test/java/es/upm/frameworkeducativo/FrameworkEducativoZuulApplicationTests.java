1e65eef8472b03844fb2263d6d949d31e03c3707
tronxi <sergio.gsanchez97@gmail.com>
2020-01-02 00:59:34 +0100

    delete test

--- src/test/java/es/upm/frameworkeducativo/FrameworkEducativoZuulApplicationTests.java
+++ src/test/java/es/upm/frameworkeducativo/FrameworkEducativoZuulApplicationTests.java
@@ -1,13 +1,8 @@
 package es.upm.frameworkeducativo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FrameworkEducativoZuulApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
